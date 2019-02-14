7f9b65eb63b3c76e4f5b52f0080507a355514c3a
Azhar <azadbasis@gmail.com>
2019-02-15 02:07:19 +0600

    POSTParams

--- app/src/main/java/com/azhar/restfulwebservices/utils/HttpHelper.java
+++ app/src/main/java/com/azhar/restfulwebservices/utils/HttpHelper.java
@@ -4,6 +4,7 @@ import java.io.BufferedOutputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.OutputStreamWriter;
 import java.net.HttpURLConnection;
 import java.net.URL;
 
@@ -28,6 +29,15 @@ public class HttpHelper {
             conn.setConnectTimeout(15000);
             conn.setRequestMethod(requestPackage.getMethod());
             conn.setDoInput(true);
+
+            if (requestPackage.getMethod().equals("POST") &&
+                    encodedParams.length() > 0) {
+                conn.setDoOutput(true);
+                OutputStreamWriter writer = new OutputStreamWriter(conn.getOutputStream());
+                writer.write(requestPackage.getEncodedParams());
+                writer.flush();
+                writer.close();
+            }
             conn.connect();
 
             int responseCode = conn.getResponseCode();
